215ecf73956e4ef2f12e524e557a40742d164dab
deangoldhill <deangoldhill@gmail.com>
2026-02-22 15:26:00 +0000

    Create const.py

---
+++ custom_components/checkpoint_management/const.py
@@ -0,0 +1,16 @@
+DOMAIN = "checkpoint_management"
+CONF_POLICY_PACKAGE = "policy_package"
+CONF_VERIFY_SSL = "verify_ssl"
+
+API_ENDPOINTS = {
+    "hosts": "show-hosts",
+    "networks": "show-networks",
+    "groups": "show-groups",
+    "dynamic_objects": "show-dynamic-objects",
+    "security_zones": "show-security-zones",
+    "data_centers": "show-data-center-objects",
+    "updatable_objects": "show-updatable-objects",
+    "access_layers": "show-access-layers",
+    "access_rules": "show-access-rulebase",
+    "nat_rules": "show-nat-rulebase"
+}
