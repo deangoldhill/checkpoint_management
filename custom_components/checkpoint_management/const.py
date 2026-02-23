8ed26e04a76070be7f45815cc41e9c2fa2054798
deangoldhill <deangoldhill@gmail.com>
2026-02-23 06:02:31 +0000

    Update const.py

--- custom_components/checkpoint_management/const.py
+++ custom_components/checkpoint_management/const.py
@@ -1,6 +1,7 @@
 DOMAIN = "checkpoint_management"
 CONF_POLICY_PACKAGE = "policy_package"
 CONF_VERIFY_SSL = "verify_ssl"
+CONF_POLLING_INTERVAL = "polling_interval"
 
 API_ENDPOINTS = {
     "hosts": "show-hosts",
@@ -12,5 +13,8 @@ API_ENDPOINTS = {
     "updatable_objects": "show-updatable-objects",
     "access_layers": "show-access-layers",
     "access_rules": "show-access-rulebase",
-    "nat_rules": "show-nat-rulebase"
+    "nat_rules": "show-nat-rulebase",
+    "vpn_meshed": "show-vpn-community-meshed",
+    "vpn_star": "show-vpn-communities-star",
+    "vpn_remote_access": "show-vpn-communities-remote-access"
 }
