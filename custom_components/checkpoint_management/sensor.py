a5cbb91ed6c906b36c52a80cdd16a2f2458bc6b6
deangoldhill <deangoldhill@gmail.com>
2026-02-23 05:14:51 +0000

    Update sensor.py

--- custom_components/checkpoint_management/sensor.py
+++ custom_components/checkpoint_management/sensor.py
@@ -1,18 +1,34 @@
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.const import CONF_HOST
 from .const import DOMAIN, API_ENDPOINTS
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
-    sensors = [CheckPointSensor(coordinator, key) for key in API_ENDPOINTS.keys()]
+    host = entry.data[CONF_HOST]
+    
+    sensors = [CheckPointSensor(coordinator, key, host, entry.entry_id) for key in API_ENDPOINTS.keys()]
     async_add_entities(sensors)
 
 class CheckPointSensor(SensorEntity):
-    def __init__(self, coordinator, key):
+    def __init__(self, coordinator, key, host, entry_id):
         self.coordinator = coordinator
         self.key = key
+        self.host = host
+        self.entry_id = entry_id
         self._attr_name = f"Check Point {key.replace('_', ' ').title()} Count"
-        self._attr_unique_id = f"cp_{coordinator.name}_{key}"
+        self._attr_unique_id = f"cp_{self.entry_id}_{key}"
         self._attr_state_class = "measurement"
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.entry_id)},
+            name=f"Check Point Management ({self.host})",
+            manufacturer="Check Point",
+            model="Management Server",
+            configuration_url=f"https://{self.host}"
+        )
 
     @property
     def state(self):
